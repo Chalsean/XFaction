77cd72f3abd5c932f65910b44805616de4b1b969
Chalsean <dcwick@gmail.com>
2022-08-08 21:41:15 -0600

    cleaned up elvui nameplates for release

--- XFaction/Configuration/Nameplates.lua
+++ XFaction/Configuration/Nameplates.lua
@@ -49,77 +49,67 @@ XFG.Options.args.Nameplates = {
 							order = 3,
 							type = 'description',
 							name = '',
-							hidden = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
 						},
 						ConfederateTag = {
 							type = 'input',
 							order = 4,
 							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_CONFEDERATE'],
 							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
 							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
+							set = function(info, value) end
 						},
-						ConfederateBracketsTag = {
+						ConfederateInitialsTag = {
 							type = 'input',
 							order = 5,
-							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_CONFEDERATE_BRACKETS'],
+							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_CONFEDERATE_INITIALS'],
 							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
 							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
+							set = function(info, value) end
+						},
+						GuildInitialsTag = {
+							type = 'input',
+							order = 6,
+							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_GUILD_INITIALS'],
+							width = 'full',
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
+							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
+							set = function(info, value) end
+						},
+						MainTag = {
+							type = 'input',
+							order = 7,
+							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_MAIN'],
+							width = 'full',
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
+							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
+							set = function(info, value) end
+						},
+						MainParenthesisTag = {
+							type = 'input',
+							order = 8,
+							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_MAIN_PARENTHESIS'],
+							width = 'full',
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
+							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
+							set = function(info, value) end
 						},
 						TeamTag = {
 							type = 'input',
-							order = 6,
+							order = 9,
 							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_TEAM'],
 							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
 							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
+							set = function(info, value) end
 						},
-						TeamParenthesisTag = {
-							type = 'input',
-							order = 7,
-							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_TEAM_PARENTHESIS'],
-							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
-							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
-						},
-						ConfederateTeamTag = {
-							type = 'input',
-							order = 8,
-							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_CONFEDERATE_TEAM'],
-							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
-							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
-						},
-						ConfederateTeamBracketsTag = {
-							type = 'input',
-							order = 9,
-							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_CONFEDERATE_TEAM_BRACKETS'],
-							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
-							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
-						},				
-						MainNameTag = {
+						MemberIcon = {
 							type = 'input',
 							order = 10,
-							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_MAIN_NAME'],
+							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_MEMBER_ICON'],
 							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
-							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
-						},
-						MainNameParenthesisTag = {
-							type = 'input',
-							order = 11,
-							name = XFG.Lib.Locale['NAMEPLATE_ELVUI_MAIN_NAME_PARENTHESIS'],
-							width = 'full',
-							disabled = function () return not XFG.Config.Nameplates.ElvUI.Enable end,
+							disabled = function () return not XFG.ElvUI or not XFG.ElvUI.private.nameplates.enable or not XFG.Config.Nameplates.ElvUI.Enable end,
 							get = function(info) return XFG.Config.Nameplates.ElvUI[ info[#info] ] end,
 							set = function(info, value) XFG.Config.Nameplates.ElvUI[ info[#info] ] = value; end
 						},	
@@ -127,77 +117,77 @@ XFG.Options.args.Nameplates = {
 				},		
 			}
 		},
-		Kui = {
-			order = 2,
-			type = 'group',
-			name = XFG.Lib.Locale['KUI'],
-			args = {
-				DHeader = {
-					order = 1,
-					type = 'group',
-					name = XFG.Lib.Locale['DESCRIPTION'],
-					inline = true,
-					args = {
-						Description = {
-							order = 1,
-							type = 'description',
-							fontSize = 'medium',
-							name = XFG.Lib.Locale['NAMEPLATE_KUI_DESCRIPTION'],
-						},
-					}
-				},
-				Options = {
-					order = 2,
-					type = 'group',
-					name = '',
-					inline = true,
-					disabled = function () return not IsAddOnLoaded('Kui_Nameplates') end,
-					args = {
-						Enable = {
-							order = 1,
-							type = 'toggle',
-							name = XFG.Lib.Locale['ENABLE'],
-							get = function(info) return XFG.Config.Nameplates.Kui[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.Kui[ info[#info] ] = value; end
-						},
-						Icon = {
-							order = 2,
-							type = 'toggle',
-							name = XFG.Lib.Locale['NAMEPLATE_KUI_ICON'],
-							desc = XFG.Lib.Locale['NAMEPLATE_KUI_ICON_TOOLTIP'],
-							disabled = function () return not XFG.Config.Nameplates.Kui.Enable end,
-							get = function(info) return XFG.Config.Nameplates.Kui[ info[#info] ] end,
-							set = function(info, value) 
-								XFG.Config.Nameplates.Kui[ info[#info] ] = value
-								if(not value) then
-									XFG.Nameplates.Kui:StripIcons()
-								end
-							end
-						},
-						Bar = {
-							order = 3,
-							name = format("|cffffffff%s|r", XFG.Lib.Locale['NAMEPLATE_KUI_GUILD_TEXT']),
-							type = 'header'
-						},						
-						GuildName = {
-							order = 4,
-							type = 'select',
-							name = XFG.Lib.Locale['NAMEPLATE_KUI_GUILD_NAME'],
-							desc = XFG.Lib.Locale['NAMEPLATE_KUI_GUILD_INITIALS_TOOLTIP'],
-							values = {
-								Confederate = XFG.Lib.Locale['CONFEDERATE'],
-								ConfederateInitials = XFG.Lib.Locale['CONFEDERATE_INITIALS'],
-								Guild = XFG.Lib.Locale['GUILD'],
-								GuildInitials = XFG.Lib.Locale['GUILD_INITIALS'],
-								Team = XFG.Lib.Locale['TEAM'],
-							},
-							disabled = function () return not XFG.Config.Nameplates.Kui.Enable end,
-							get = function(info) return XFG.Config.Nameplates.Kui[ info[#info] ] end,
-							set = function(info, value) XFG.Config.Nameplates.Kui[ info[#info] ] = value; end
-						},
-					},
-				},			
-			}
-		},
+		-- Kui = {
+		-- 	order = 2,
+		-- 	type = 'group',
+		-- 	name = XFG.Lib.Locale['KUI'],
+		-- 	args = {
+		-- 		DHeader = {
+		-- 			order = 1,
+		-- 			type = 'group',
+		-- 			name = XFG.Lib.Locale['DESCRIPTION'],
+		-- 			inline = true,
+		-- 			args = {
+		-- 				Description = {
+		-- 					order = 1,
+		-- 					type = 'description',
+		-- 					fontSize = 'medium',
+		-- 					name = XFG.Lib.Locale['NAMEPLATE_KUI_DESCRIPTION'],
+		-- 				},
+		-- 			}
+		-- 		},
+		-- 		Options = {
+		-- 			order = 2,
+		-- 			type = 'group',
+		-- 			name = '',
+		-- 			inline = true,
+		-- 			disabled = function () return not IsAddOnLoaded('Kui_Nameplates') end,
+		-- 			args = {
+		-- 				Enable = {
+		-- 					order = 1,
+		-- 					type = 'toggle',
+		-- 					name = XFG.Lib.Locale['ENABLE'],
+		-- 					get = function(info) return XFG.Config.Nameplates.Kui[ info[#info] ] end,
+		-- 					set = function(info, value) XFG.Config.Nameplates.Kui[ info[#info] ] = value; end
+		-- 				},
+		-- 				Icon = {
+		-- 					order = 2,
+		-- 					type = 'toggle',
+		-- 					name = XFG.Lib.Locale['NAMEPLATE_KUI_ICON'],
+		-- 					desc = XFG.Lib.Locale['NAMEPLATE_KUI_ICON_TOOLTIP'],
+		-- 					disabled = function () return not XFG.Config.Nameplates.Kui.Enable end,
+		-- 					get = function(info) return XFG.Config.Nameplates.Kui[ info[#info] ] end,
+		-- 					set = function(info, value) 
+		-- 						XFG.Config.Nameplates.Kui[ info[#info] ] = value
+		-- 						if(not value) then
+		-- 							XFG.Nameplates.Kui:StripIcons()
+		-- 						end
+		-- 					end
+		-- 				},
+		-- 				Bar = {
+		-- 					order = 3,
+		-- 					name = format("|cffffffff%s|r", XFG.Lib.Locale['NAMEPLATE_KUI_GUILD_TEXT']),
+		-- 					type = 'header'
+		-- 				},						
+		-- 				GuildName = {
+		-- 					order = 4,
+		-- 					type = 'select',
+		-- 					name = XFG.Lib.Locale['NAMEPLATE_KUI_GUILD_NAME'],
+		-- 					desc = XFG.Lib.Locale['NAMEPLATE_KUI_GUILD_INITIALS_TOOLTIP'],
+		-- 					values = {
+		-- 						Confederate = XFG.Lib.Locale['CONFEDERATE'],
+		-- 						ConfederateInitials = XFG.Lib.Locale['CONFEDERATE_INITIALS'],
+		-- 						Guild = XFG.Lib.Locale['GUILD'],
+		-- 						GuildInitials = XFG.Lib.Locale['GUILD_INITIALS'],
+		-- 						Team = XFG.Lib.Locale['TEAM'],
+		-- 					},
+		-- 					disabled = function () return not XFG.Config.Nameplates.Kui.Enable end,
+		-- 					get = function(info) return XFG.Config.Nameplates.Kui[ info[#info] ] end,
+		-- 					set = function(info, value) XFG.Config.Nameplates.Kui[ info[#info] ] = value; end
+		-- 				},
+		-- 			},
+		-- 		},			
+		-- 	}
+		--},
 	}
 }
--- XFaction/NamePlates/ElvUI.lua
+++ XFaction/NamePlates/ElvUI.lua
@@ -12,101 +12,75 @@ end
 
 function ElvUINameplate:Initialize()
 	if(not self:IsInitialized()) then
+        if(XFG.Config.Nameplates.ElvUI.Enable) then
 
-        XFG.ElvUI:AddTag('confederate', 'UNIT_NAME_UPDATE PLAYER_GUILD_UPDATE', function(inNameplate) 
-            if(UnitIsPlayer(inNameplate)) then
-                local _GuildName = XFG.Player.Guild:GetName()
-                if(XFG.Config.Nameplates.Confederate.Enable) then
+            XFG.ElvUI:AddTag('confederate', 'UNIT_NAME_UPDATE PLAYER_GUILD_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                local _GuildName = GetGuildInfo(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
                     _GuildName = XFG.Confederate:GetName()
                 end
                 return _GuildName
-            end 
-        end)
+            end)
 
-        XFG.ElvUI:AddTag('confederate:brackets', 'PLAYER_GUILD_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            local _GuildName = GetGuildInfo(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                _GuildName = XFG.Confederate:GetName()
-            end
-            if(_GuildName) then
-                return format('<%s>', _GuildName)
-            end 
-        end)
-        
-        XFG.ElvUI:AddTag('name:main', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            local _UnitName = UnitName(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                local _UnitData = XFG.Confederate:GetUnit(_GUID)
-                if(_UnitData:HasMainName()) then
-                    _UnitName = _UnitData:GetMainName()
+            XFG.ElvUI:AddTag('confederate:initials', 'PLAYER_GUILD_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                local _GuildName = GetGuildInfo(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
+                    _GuildName = XFG.Confederate:GetKey()
                 end
-            end
-            return _UnitName
-        end)
-        
-        XFG.ElvUI:AddTag('name:main-paren', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                local _UnitData = XFG.Confederate:GetUnit(_GUID)
-                if(_UnitData:HasMainName()) then
-                    return '(' .. _UnitData:GetMainName() .. ')'
-                end
-            end
-        end)
-        
-        XFG.ElvUI:AddTag('team', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                return XFG.Confederate:GetUnit(_GUID):GetTeam():GetName()
-            end
-        end)
-        
-        XFG.ElvUI:AddTag('team-paren', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                return '(' .. XFG.Confederate:GetUnit(_GUID):GetTeam():GetName() .. ')'
-            end
-        end)
-        
-        XFG.ElvUI:AddTag('confederate:team', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            local _GuildName = GetGuildInfo(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                _GuildName = XFG.Confederate:GetName() .. ':' .. XFG.Confederate:GetUnit(_GUID):GetTeam():GetName()
-            end
-            return _GuildName
-        end)
-        
-        XFG.ElvUI:AddTag('confederate:team:brackets', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            local _GuildName = GetGuildInfo(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                _GuildName = XFG.Confederate:GetName() .. ':' .. XFG.Confederate:GetUnit(_GUID):GetTeam():GetName()
-            end
-            if(_GuildName) then
-                return '<' .. _GuildName .. '>'
-            end
-        end)
+                if(_GuildName) then
+                    return format('<%s>', _GuildName)
+                end 
+            end)
 
-        XFG.ElvUI:AddTag('confederate-init:team:brackets', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            local _GuildName = GetGuildInfo(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                _GuildName = XFG.Confederate:GetKey() .. ':' .. XFG.Confederate:GetUnit(_GUID):GetTeam():GetName()
-            end
-            if(_GuildName) then
-                return '<' .. _GuildName .. '>'
-            end
-        end)
-        
-        XFG.ElvUI:AddTag('confederate:icon', 'UNIT_NAME_UPDATE', function(inNameplate) 
-            local _GUID = UnitGUID(inNameplate)
-            if(XFG.Confederate:Contains(_GUID)) then
-                return XFG.Icons.Guild
-            end
-        end)
+            XFG.ElvUI:AddTag('guild:initials', 'PLAYER_GUILD_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                local _GuildName = GetGuildInfo(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
+                    _GuildName = XFG.Confederate:GetUnit(_GUID):GetGuild():GetInitials()
+                end
+                if(_GuildName) then
+                    return format('<%s>', _GuildName)
+                end 
+            end)
+            
+            XFG.ElvUI:AddTag('main', 'UNIT_NAME_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                local _UnitName = UnitName(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
+                    local _UnitData = XFG.Confederate:GetUnit(_GUID)
+                    if(_UnitData:HasMainName()) then
+                        _UnitName = _UnitData:GetMainName()
+                    end
+                end
+                return _UnitName
+            end)
+            
+            XFG.ElvUI:AddTag('main:parenthesis', 'UNIT_NAME_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
+                    local _UnitData = XFG.Confederate:GetUnit(_GUID)
+                    if(_UnitData:HasMainName()) then
+                        return '(' .. _UnitData:GetMainName() .. ')'
+                    end
+                end
+            end)
+            
+            XFG.ElvUI:AddTag('team', 'UNIT_NAME_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
+                    return XFG.Confederate:GetUnit(_GUID):GetTeam():GetName()
+                end
+            end)
+            
+            XFG.ElvUI:AddTag('confederate:icon', 'UNIT_NAME_UPDATE', function(inNameplate) 
+                local _GUID = UnitGUID(inNameplate)
+                if(XFG.Confederate:Contains(_GUID)) then
+                    return XFG.Icons.Guild
+                end
+            end)
+        end
         
 		self:IsInitialized(true)
 	end
