3862e01d1b851a1c83b18d03100d7d32efb7e915
Chalsean <dcwick@gmail.com>
2023-05-04 08:02:17 -0600

    prod release

--- XFaction/Configuration/ChangeLog/4.5.6.lua
+++ XFaction/Configuration/ChangeLog/4.5.6.lua
@@ -1,7 +1,7 @@
 local XFG, G = unpack(select(2, ...))
 local ObjectName = 'Config.ChangeLog'
 
-XFG.ChangeLog['4.5.5'] = {
+XFG.ChangeLog['4.5.6'] = {
     Improvements = {
         order = 2,
         type = 'group',
@@ -12,7 +12,7 @@ XFG.ChangeLog['4.5.5'] = {
                 order = 1,
                 type = 'description',
                 fontSize = 'medium',
-                name = 'Fixed Guild (X) showing wrong faction for players, was showing the guild faction that player was in.',
+                name = 'Fixed unit faction recognition when it is different than the guild.',
             },
         },
     },			
