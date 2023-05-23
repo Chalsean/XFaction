6f995f666406ce685a4fb9142a263008f3b3c9d8
Chalsean <dcwick@gmail.com>
2023-05-23 12:24:51 -0600

    packaged for release

--- XFaction/Configuration/ChangeLog/4.6.2.lua
+++ XFaction/Configuration/ChangeLog/4.6.2.lua
@@ -1,11 +1,11 @@
 local XFG, G = unpack(select(2, ...))
 local ObjectName = 'Config.ChangeLog'
 
-XFG.ChangeLog['4.6.1'] = {
+XFG.ChangeLog['4.6.2'] = {
     Improvements = {
         order = 2,
         type = 'group',
-        name = XFG.Lib.Locale['IMPROVEMENTS'],
+        name = XFG.Lib.Locale['NEW'],
         guiInline = true,
         args = {
             One = {
