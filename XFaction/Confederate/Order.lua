c96d02ea6856844eb2f505fcf199e1423fb74f40
Dave Wick <dcwick@gmail.com>
2023-08-16 11:37:22 -0600

    Order and OrderCollection classes

--- XFaction/Confederate/Order.lua
+++ XFaction/Confederate/Order.lua
@@ -14,7 +14,20 @@ function Order:new()
     object.minQuality = 1
     object.itemHyperlink = nil
     object.itemGUID = nil
+    object.isMine = false
     return object
+end
+
+function Order:Deconstructor()
+    self:ParentDeconstructor()
+    self.ID = 0
+    self.itemID = nil
+    self.isFulfillable = nil
+    self.customerGUID = nil
+    self.minQuality = 1
+    self.itemHyperlink = nil
+    self.itemGUID = nil
+    self.isMine = false
 end
 --#endregion
 
@@ -22,7 +35,13 @@ end
 function Order:Print()
     self:ParentPrint()
     XFG:Debug(ObjectName, '  ID (' .. type(self.ID) .. '): ' .. tostring(self.ID))
-    XFG:Debug(ObjectName, '  iconID (' .. type(self.iconID) .. '): ' .. tostring(self.iconID))
+    XFG:Debug(ObjectName, '  itemID (' .. type(self.itemID) .. '): ' .. tostring(self.itemID))
+    XFG:Debug(ObjectName, '  isFulfillable (' .. type(self.isFulfillable) .. '): ' .. tostring(self.isFulfillable))
+    XFG:Debug(ObjectName, '  customerGUID (' .. type(self.customerGUID) .. '): ' .. tostring(self.customerGUID))
+    XFG:Debug(ObjectName, '  minQuality (' .. type(self.minQuality) .. '): ' .. tostring(self.minQuality))
+    XFG:Debug(ObjectName, '  itemHyperlink (' .. type(self.itemHyperlink) .. '): ' .. tostring(self.itemHyperlink))
+    XFG:Debug(ObjectName, '  itemGUID (' .. type(self.itemGUID) .. '): ' .. tostring(self.itemGUID))
+    XFG:Debug(ObjectName, '  isMine (' .. type(self.isMine) .. '): ' .. tostring(self.isMine))
 end
 --#endregion
 
@@ -31,17 +50,69 @@ function Order:GetID()
     return self.ID
 end
 
-function Order:SetID(inOrderID)
-    assert(type(inOrderID) == 'number')
-    self.ID = inOrderID
+function Order:SetID(inID)
+    assert(type(inID) == 'number')
+    self.ID = inID
 end
 
-function Order:GetIconID()
-    return self.iconID
+function Order:GetItemID()
+    return self.itemID
 end
 
-function Order:SetIconID(inIconID)
-    assert(type(inIconID) == 'number')
-    self.iconID = inIconID
+function Order:SetItemID(inItemID)
+    assert(type(inItemID) == 'number')
+    self.itemID = inItemID
+end
+
+function Order:IsFulfillable(inBoolean)
+    assert(type(inBoolean) == 'boolean' or inBoolean == nil, 'argument must be boolean or nil')
+    if(inBoolean ~= nil) then
+        self.isFulfillable = inBoolean
+    end    
+    return self.isFulfillable
+end
+
+function Order:GetCustomerGUID()
+    return self.customerGUID
+end
+
+function Order:SetCustomerGUID(inGUID)
+    assert(type(inGUID) == 'string')
+    self.customerGUID = inGUID
+end
+
+function Order:GetMinimumQuantity()
+    return self.minQuality
+end
+
+function Order:SetMinimumQuantity(inQuantity)
+    assert(type(inQuantity) == 'number' and inQuantity >= 0)
+    self.minQuality = inQuantity
+end
+
+function Order:GetItemHyperlink()
+    return self.itemHyperlink
+end
+
+function Order:SetItemHyperlink(inLink)
+    assert(type(inLink) == 'string')
+    self.itemHyperlink = inLink
+end
+
+function Order:GetItemGUID()
+    return self.itemGUID
+end
+
+function Order:SetItemGUID(inGUID)
+    assert(type(inGUID) == 'string')
+    self.itemGUID = inGUID
+end
+
+function Order:IsMyOrder(inBoolean)
+    assert(type(inBoolean) == 'boolean' or inBoolean == nil, 'argument must be boolean or nil')
+    if(inBoolean ~= nil) then
+        self.isMine = inBoolean
+    end    
+    return self.isMine
 end
 --#endregion
--- XFaction/Confederate/OrderCollection.lua
+++ XFaction/Confederate/OrderCollection.lua
@@ -1,10 +1,7 @@
 local XFG, G = unpack(select(2, ...))
 local ObjectName = 'OrderCollection'
-local GetAllProfessionIDs = C_TradeSkillUI.GetAllProfessionTradeSkillLines
-local GetProfessionName = C_TradeSkillUI.GetTradeSkillDisplayName
-local GetProfessionIcon = C_TradeSkillUI.GetTradeSkillTexture
 
-OrderCollection = ObjectCollection:newChildConstructor()
+OrderCollection = Factory:newChildConstructor()
 
 --#region Constructors
 function OrderCollection:new()
@@ -12,22 +9,16 @@ function OrderCollection:new()
 	object.__name = ObjectName
     return object
 end
+
+function OrderCollection:NewObject()
+	return Order:new()
+end
 --#endregion
 
 --#region Initializers
 function OrderCollection:Initialize()
 	if(not self:IsInitialized()) then
 		self:ParentInitialize()
-		for _, ID in ipairs (GetAllProfessionIDs()) do
-			local name = GetProfessionName(ID)
-			local profession = Profession:new()
-			profession:SetID(ID)
-			profession:SetIconID(GetProfessionIcon(ID))
-			profession:SetName(GetProfessionName(ID))
-			profession:SetKey(ID)
-			self:Add(profession)
-			XFG:Info(ObjectName, 'Initialized profession [%d:%s]', profession:GetID(), profession:GetName())
-		end
 		self:IsInitialized(true)
 	end
 end
