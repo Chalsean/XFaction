41a51a8dc0d5525b00672c36ebd3e873d10e9762
Chalsean <dcwick@gmail.com>
2023-10-01 16:32:27 -0600

    updated to latest Ace3 libraries

--- XFaction/UI/Configuration/ChangeLog/4.7.0.lua
+++ XFaction/UI/Configuration/ChangeLog/4.7.0.lua
@@ -12,10 +12,16 @@ XF.ChangeLog['4.7.0'] = {
                 order = 1,
                 type = 'description',
                 fontSize = 'medium',
-                name = 'Added notifications for guild/personal crafting orders. Both sender and receiver must be running XFaction and in the same confederate. These can be enabled/disabled under Options, XFaction, Chat, Crafting.',
+                name = 'Added notifications for guild/personal crafting orders. Both sender and receiver must be running XFaction and in the same confederate.',
             },
             Two = {
                 order = 2,
+                type = 'description',
+                fontSize = 'medium',
+                name = 'Crafting notifications can be enabled/disabled under Options, XFaction, Chat, Crafting.',
+            },
+            Three = {
+                order = 3,
                 type = 'description',
                 fontSize = 'medium',
                 name = 'Bumped TOC to 100107.',
