14969d96055cc54eba58ae77091711e1a40aca4c
Chalsean <dcwick@gmail.com>
2023-11-18 23:28:33 -0700

    Create 4.8.2.lua

---
+++ XFaction/UI/Configuration/ChangeLog/4.8.2.lua
@@ -0,0 +1,33 @@
+local XF, G = unpack(select(2, ...))
+local ObjectName = 'Config.ChangeLog'
+
+XF.ChangeLog['4.8.2'] = {
+    New = {
+        order = 1,
+        type = 'group',
+        name = XF.Lib.Locale['NEW'],
+        guiInline = true,
+        args = {
+            One = {
+                order = 1,
+                type = 'description',
+                fontSize = 'medium',
+                name = 'Mythic keystones are now visible in Guild (X) DT.',
+            },
+        },
+    },	
+    Improvements = {
+        order = 2,
+        type = 'group',
+        name = XF.Lib.Locale['IMPROVEMENTS'],
+        guiInline = true,
+        args = {
+            One = {
+                order = 1,
+                type = 'description',
+                fontSize = 'medium',
+                name = 'Updated to latest LibTourist libraries.',
+            },
+        },
+    },		
+}
