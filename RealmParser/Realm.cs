79b78143c6de3f6d4ef46e22f3ffd1b48b385a00
Dave Wick <dcwick@gmail.com>
2024-08-13 14:21:07 -0600

    added realm parser

---
+++ RealmParser/Realm.cs
@@ -0,0 +1,8 @@
+﻿public sealed record Realm
+{
+    public int RootRealmID { get; set; } = 0;
+    public int RealmID { get; set; } = 0;
+    public string RegionName { get; set; } = string.Empty;
+    public Dictionary<string, string> RealmName { get; set; } = new();
+    public string GameVersion { get; set; } = string.Empty;
+}
