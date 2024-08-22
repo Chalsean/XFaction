feea55463e610d7c7e2e45b74df733a119b129e9
Chalsean <dcwick@gmail.com>
2024-08-22 12:09:32 -0600

    removed old files

--- RealmParser/Realm.cs
+++
@@ -1,8 +0,0 @@
-﻿public sealed record Realm
-{
-    public int RootRealmID { get; set; } = 0;
-    public int RealmID { get; set; } = 0;
-    public string RegionName { get; set; } = string.Empty;
-    public Dictionary<string, string> RealmName { get; set; } = new();
-    public string GameVersion { get; set; } = string.Empty;
-}
