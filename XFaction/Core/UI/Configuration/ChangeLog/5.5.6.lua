b4f696022d3c74c6d401b9b2f6138a011c26963b
chals <dcwick@gmail.com>
2025-12-26 11:02:33 -0700

    5.5.6

---
+++ XFaction/Core/UI/Configuration/ChangeLog/5.5.6.lua
@@ -0,0 +1,19 @@
+local XF, G = unpack(select(2, ...))
+local ObjectName = 'Config.ChangeLog'
+
+XF.ChangeLog['5.5.6'] = {
+    Improvements = {
+        order = 2,
+        type = 'group',
+        name = XF.Lib.Locale['IMPROVEMENTS'],
+        guiInline = true,
+        args = {
+            Localization = {
+                order = 1,
+                type = 'description',
+                fontSize = 'medium',
+                name = 'Added Russian translation provided by Hubbotu',
+            },
+        },
+    },		
+}
