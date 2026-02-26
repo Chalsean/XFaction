377442f214ab6f123c5934fd1187ad7f8b61fa2e
chals <dcwick@gmail.com>
2026-02-26 15:43:04 -0700

    change log

---
+++ XFaction/Core/UI/Configuration/ChangeLog/6.0.7.lua
@@ -0,0 +1,25 @@
+local XF, G = unpack(select(2, ...))
+local ObjectName = 'Config.ChangeLog'
+
+XF.ChangeLog['6.0.7'] = {
+    Improvements = {
+        order = 2,
+        type = 'group',
+        name = XF.Lib.Locale['IMPROVEMENTS'],
+        guiInline = true,
+        args = {
+            Deprecated = {
+                order = 1,
+                type = 'description',
+                fontSize = 'medium',
+                name = 'Hopefully fixed online/offline notifications for real this time (thx Hyphie!)',
+            },
+            Duplicate = {
+                order = 2,
+                type = 'description',
+                fontSize = 'medium',
+                name = 'Added logic to log out of chat channel when logging off or leaving guild. People were getting password prompts',
+            },
+        },
+    },		
+}
